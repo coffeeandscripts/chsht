3621ad6cec1dc80f1e7c75dd8c1f69b55eaeeeb2
cindyjbli <cindycinders@gmail.com>
2018-08-29 01:28:55 +1000

    Interpreter: implementing buffer on  commands

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <cstring>
+#include <iostream> #delete later
 
 #include "project/interpreter.hpp"
 
@@ -7,7 +8,17 @@ int main (int argc, char *argv[]) {
         Interpreter c(argc, argv[]);
         if (c.check_args()) {
                 if(c.eval_args()) {
-                        system("sed -i '2s/.*/less -FX .\/docs\/sheets\/vim.chsht/' ./docs/sheets/open.sh" | ./docs./sheets/open.sh );
+                        //just testing with a set input first, then can take arg input
+                        //i found this on the internet
+                        char a[] = "vim";
+                        char buf[150];
+                        //for snprintf??? char buf[BUFSIZ]
+                        // or use snprintf to prevent buffer being too small??? snprintf(buf, sizeof(buf) , "text %s", a)
+                        sprintf(buf, "sed -i '2s/.*/less -FX .\/docs\/sheets\/%s.chsht/' ./docs/sheets/open.sh" | ./docs./sheets/open.sh", a);
+                        //want to check if this got stored correctly
+                        std::cout << "buffer is:   " << buf << endl;
+                        //theoreticall this should work?
+                        system(buf);
                 }
         }
         return 0;
