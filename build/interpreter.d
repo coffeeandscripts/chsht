2138c818a37c17bd120a36dce6ed239ecd92b49e
cindyjbli <cindycinders@gmail.com>
2018-08-29 23:36:26 +1000

    Optimised memory use by system() function

--- build/interpreter.d
+++
@@ -1,3 +0,0 @@
-build/interpreter.o: src/interpreter.cpp include/project/interpreter.hpp
-
-include/project/interpreter.hpp:
--- build/main.d
+++
@@ -1,3 +0,0 @@
-build/main.o: src/main.cpp include/project/interpreter.hpp
-
-include/project/interpreter.hpp:
