14d1f2a5bea43d4ba62d8b575dd3c76f68ac200c
coffeeandscripts <erin.saricilar@gmail.com>
2018-09-07 00:44:58 +1000

    Cleaned

--- build/instructions.d
+++
@@ -1,4 +0,0 @@
-build/instructions.o: src/instructions.cpp \
- include/project/instructions.hpp
-
-include/project/instructions.hpp:
--- build/interpreter.d
+++
@@ -1,6 +0,0 @@
-build/interpreter.o: src/interpreter.cpp include/project/interpreter.hpp \
- include/project/setup.hpp
-
-include/project/interpreter.hpp:
-
-include/project/setup.hpp:
